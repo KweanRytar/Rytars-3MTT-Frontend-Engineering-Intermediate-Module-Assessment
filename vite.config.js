60782dce5c45fcd809de76052bd11e17407b10be
Rytar <ritanoblejane@gmail.com>
2025-02-26 18:07:42 +0100

    Configured GitHub Pages Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Rytars-3MTT-Frontend-Engineering-Intermediate-Module-Assessment/", 
 })
